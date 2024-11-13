16fd0fa3fded822ebb6ba3fd83657a71631d1829
yago <yagosonicmassaroni@gmail.com>
2024-11-13 15:54:52 -0300

    pull

--- index.js
+++ index.js
@@ -16,4 +16,3 @@ var containsNearbyDuplicate = function (nums, k) {
 
 console.log(containsNearbyDuplicate([1, 0, 1, 1], 1))
 console.log(containsNearbyDuplicate([1, 2, 3, 1],  3))
-console.log(containsNearbyDuplicate([1, 2, 3, 1, 2, 3], 2))
